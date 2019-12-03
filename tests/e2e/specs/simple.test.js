4f03f265afeb0b12e0eac3f42d8217b359804a6b
Connor Jennings <connor.d.jennings@jpmchase.com>
2019-12-03 15:48:05 -0500

    Add e2e testing to Edit Flow

---
+++ tests/e2e/specs/simple.test.js
@@ -0,0 +1,18 @@
+/**
+ * WordPress dependencies
+ */
+
+import { visitAdminPage, createNewPost } from "@wordpress/e2e-test-utils";
+
+describe("Edit Flow", () => {
+  beforeAll(async () => {
+    await visitAdminPage("admin.php", "page=ef-settings");
+  });
+
+  it("the plugin settings page loads", async () => {
+    const editFlow = await page.$(".edit-flow-admin h2");
+    const html = await page.evaluate(body => body.innerHTML, editFlow);
+
+    expect(html).toEqual("Edit Flow");
+  });
+});
