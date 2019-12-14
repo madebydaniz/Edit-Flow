a783ad4adf05d8f0cff4c6940d6679791aa8fdd3
Connor Jennings <cojennin@gmail.com>
2019-12-14 16:58:35 -0500

    Support i18n in story budget date picker

--- common/js/ef_date.js
+++ common/js/ef_date.js
@@ -1,23 +1,29 @@
-jQuery(document).ready(function($) {
-  var dateTimePicks = jQuery(".date-time-pick");
+jQuery( document ).ready( function( $ ) {
+  var dateTimePicks = jQuery( '.date-time-pick' );
 
-  dateTimePicks.each(function() {
-    var $dTP = jQuery(this);
+	dateTimePicks.each( function() {
+		var $dTP = jQuery(this);
 
-    $dTP.datetimepicker({
-      dateFormat: "M dd yy",
-      firstDay: ef_week_first_day,
-      alwaysSetTime: false,
-      controlType: "select",
-      altField: '#' + $dTP.prop("id") + "_hidden",
-      altFieldTimeOnly: false,
-      altFormat: "yy-mm-dd",
-      altTimeFormat: "HH:mm"
-    });
-  });
+		$dTP.datetimepicker( {
+			dateFormat: 'M dd yy',
+			firstDay: ef_week_first_day,
+			alwaysSetTime: false,
+			controlType: 'select',
+			altField: '#' + $dTP.prop( 'id' ) + '_hidden',
+			altFieldTimeOnly: false,
+			altFormat: 'yy-mm-dd',
+			altTimeFormat: 'HH:mm',
+		} );
+	} );
 
-  jQuery(".date-pick").datepicker({
-    dateFormat: "M dd yy",
-    firstDay: ef_week_first_day
-  });
-});
+	var datePicks = jQuery( '.date-pick' );
+	datePicks.each( function() {
+		var $datePicker = jQuery( this );
+
+		$datePicker.datepicker( {
+			firstDay: ef_week_first_day,
+			altField: '#' + $datePicker.prop( 'id' ) + '_hidden',
+			altFormat: 'yy-mm-dd',
+		} );
+	} );
+} );
